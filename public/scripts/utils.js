a01fb1933f0d4acc10b6bd77e85235f6233d1819
HP <31686938+yougo96@users.noreply.github.com>
2024-11-28 20:05:39 +0100

    Dialog add next previous image

--- public/scripts/utils.js
+++ public/scripts/utils.js
@@ -1,16 +1,16 @@
 
-function toggleModal(id, data) {
+function toggleModal(id, index) {
     const modal = document.getElementById(id)
     if (!modal) return
-    modal && (modal.open ? modal.close() : modal.showModal());
+    if (modal.open) {
+        modal.close()
+    } 
+    else {
+        modal.showModal()
+        // modal.focus()
+        window.location.hash = modal;
+    }
 
-    modal.querySelector('img').src = data.baseUrl + "=w2048-h1024";
-    modal.querySelectorAll('.img-info > li > b')[0].innerText = data.filename;
-    modal.querySelectorAll('.img-info > li > b')[1].innerText = data.mediaMetadata.creationTime;
-    modal.querySelectorAll('.img-info > li > b')[2].innerText = data.mediaMetadata.photo.cameraMake;
-    modal.querySelectorAll('.img-info > li > b')[3].innerText = data.mediaMetadata.photo.cameraModel;
-    modal.querySelectorAll('.img-info > li > b')[4].innerText = data.mediaMetadata.photo.focalLength;
-    modal.querySelectorAll('.img-info > li > b')[5].innerText = data.mediaMetadata.photo.apertureFNumber
-    modal.querySelectorAll('.img-info > li > b')[6].innerText = data.mediaMetadata.photo.isoEquivalent;
-    modal.querySelectorAll('.img-info > li > b')[7].innerText = data.mediaMetadata.photo.exposureTime;
+
+    if (index) setModalIndex(modal, index)
 }
